d2b189bd54ed504577f5d756f284fb223aaca157
malcolmtan <zherui_tan@live.com.sg>
2023-10-26 11:31:51 +0800

    Update Hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,39 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: {
+    version: "0.8.0",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    bsctestnet: {
+      url: "https://data-seed-prebsc-2-s1.bnbchain.org:8545	",
+      chainId: 97,
+      gas: 12400000,
+      gasPrice: 20000000000,
+      accounts: {
+        mnemonic: MNEMONIC
+      },
+      networkCheckTimeout: 999999,
+      timeoutBlocks: 200,
+    },
+    bscmainnet: {
+      url: "https://bsc-dataseed.bnbchain.org/",
+      chainId: 56,
+      gasPrice: 10000000000,
+      accounts: {
+        mnemonic: MNEMONIC
+      },
+    },
+  },
+  etherscan: {
+    apiKey: process.env.BSCSCANAPI
+  }
 };
