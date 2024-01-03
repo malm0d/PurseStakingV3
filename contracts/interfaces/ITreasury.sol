93aba5a3d3ada3cbc70a2bb205610d99f90699c0
malcolmtan <zherui_tan@live.com.sg>
2024-01-03 16:41:23 +0800

    Update PurseStaking Treasury interface for upgrade

--- contracts/interfaces/ITreasury.sol
+++ contracts/interfaces/ITreasury.sol
@@ -3,4 +3,5 @@ pragma solidity ^0.8.0;
 
 interface ITreasury {
     function updateUserAvailableRewards(address _address, uint256 _amount) external;
+    function claimRewards(address _address) external returns (uint256);
 }
