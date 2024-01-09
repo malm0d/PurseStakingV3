e896ab9cec07b1a279444465e153157a82e3bb77
malcolmtan <zherui_tan@live.com.sg>
2024-01-09 16:15:40 +0800

    Update IPurseStakingVesting interface for vesting contract

---
+++ contracts/interfaces/IPurseStakingVesting.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+interface IPurseStakingVesting {
+    struct VestingSchedule {
+        uint128 startTime;
+        uint128 endTime;
+        uint256 quantity;
+        uint256 vestedQuantity;
+    }
+
+    function lockWithEndTime(address account, uint256 quantity, uint256 endTime) external;
+
+    function getVestingSchedules(address account) external view returns (VestingSchedule[] memory);
+
+    function accountEscrowedBalance(address account) external view returns (uint256);
+
+    function accountVestedBalance(address account) external view returns (uint256);
+}
