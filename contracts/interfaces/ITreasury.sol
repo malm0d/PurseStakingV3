ad146a6e45eab5f6b8e5c515ff7a8f37a582547b
malcolmtan <zherui_tan@live.com.sg>
2024-01-25 17:36:49 +0800

    Update Treasury interface

--- contracts/interfaces/ITreasury.sol
+++ contracts/interfaces/ITreasury.sol
@@ -3,5 +3,5 @@ pragma solidity ^0.8.0;
 
 interface ITreasury {
     function updateUserAvailableRewards(address _address, uint256 _amount) external;
-    function claimRewardsV2(address _address) external returns (uint256);
+    function claimRewards(address _address) external returns (uint256);
 }
